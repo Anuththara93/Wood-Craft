0aab00e2b00dfb9681d7bf2bb389a5f69f578227
Anuththara93 <2021cs009@stu.ucsc.cmb.ac.lk>
2023-12-24 00:17:52 +0530

    add profile picture

--- profile_page/profile_page.js
+++ profile_page/profile_page.js
@@ -11,16 +11,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const overviewTabTitle = document.querySelector('.tab-title[data-tab="profile-overview"]');
     overviewTabTitle.classList.add('active');
 
+    const saveChangesBtn = document.getElementById('saveChangesBtn');
+    saveChangesBtn.addEventListener('click', function () {
+
+        showTab('profile-overview');
+    });
+
     function showTab(tabId) {
         const tabTitles = document.querySelectorAll('.tab-title');
-        tabTitles.forEach(title => {
-            title.classList.remove('active');
-        });
-
         const tabPanes = document.querySelectorAll('.tab-pane');
-        tabPanes.forEach(pane => {
-            pane.classList.remove('active');
-        });
+
+        tabTitles.forEach(title => title.classList.remove('active'));
+        tabPanes.forEach(pane => pane.classList.remove('active'));
 
         const selectedTabTitle = document.querySelector(`.tab-title[data-tab="${tabId}"]`);
         const selectedTabPane = document.getElementById(tabId);
@@ -28,9 +30,50 @@ document.addEventListener('DOMContentLoaded', function () {
         selectedTabTitle.classList.add('active');
         selectedTabPane.classList.add('active');
 
-        overviewTabTitle.classList.remove('active');
+        if (tabId === 'profile-edit') {
+            const profilePictureInput = document.getElementById('profilePicture');
+            profilePictureInput.addEventListener('change', handleProfilePictureChange);
+        }
     }
 });
+
+function handleProfilePictureChange(event) {
+    const fileInput = event.target;
+    const file = fileInput.files[0];
+
+    if (file) {
+        const profileImagePreview = document.querySelector('.profile-container img');
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            profileImagePreview.src = e.target.result;
+        };
+
+        reader.readAsDataURL(file);
+    }
+}
+
+function displayImage(input) {
+    const file = input.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            const imageElement = document.querySelector('.col-md-8 .col-lg-9 img');
+            imageElement.src = e.target.result;
+        };
+
+        reader.readAsDataURL(file);
+    }
+}
+
+function removeProfileImage() {
+    const imageElement = document.querySelector('.col-md-8 .col-lg-9 img');
+    imageElement.src = 'image/profile-img.png';
+    document.getElementById('uploadInput').value = '';
+}
+
 
   
   
