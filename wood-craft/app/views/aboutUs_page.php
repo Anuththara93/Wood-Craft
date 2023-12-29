5e4f8f09f0eb08d19db376df365491efb21aef44
Anuththara93 <2021cs009@stu.ucsc.cmb.ac.lk>
2023-12-29 22:59:38 +0530

    do small changes

--- wood-craft/app/views/aboutUs_page.php
+++ wood-craft/app/views/aboutUs_page.php
@@ -13,48 +13,7 @@
 <body>
     <main id="main" class="main">
 
-        <div class="card">
-            <div class="title-container">
-                <h1>Make Your Interior Minimalist & Modern</h1>
-                <h3>Delivering Comfort as a Priority: Quick and Easy Access to a Wide Variety of Furniture</h3>
-
-            </div>
-
-            <div class="image-container">
-                
-            </div>
-
-        </div>
-
-        <div class="card 1">
-            <div class="tab-container">
-                <h1>What do we do...?</h1>
-                <p>With our well experienced craftsmen, our talented designers and our years of professional experience and expertise, 
-                    we can create interiors that are stimulating, and sophisticated. We have a range of designs that have successfully 
-                    become part of our portfolio and we constantly update ourselves with the current design trends and styles, in order 
-                    to provide even fresher creations for our customers!</p>
-
-                <h2>Our Vision</h2>
-                <p>We have a vision to be the best in providing total solutions in furniture and interiors to our clientele, with an 
-                    aim to enhance their own perceptions on functional and innovative design. We envision designs that elevate their 
-                    living and working environment, in a way that only we can do.</p>
-
-                <h2>Our Mission</h2>
-                <p>We are on a mission to recognize our clients’ needs and wants and fulfill them with dedication and hard work. 
-                    Furniture Plus insists on the delivery of quality and we strive to enrich our business by building a strong 
-                    relationship with our clients and our industry partners alike.</p>
-
-            </div>
-
-            <div class="logo-container">
-                
-            </div>
-
-        </div>
-
-        <div class="card 2">
-                
-        </div>
+       
 
     </main>
 
